659d562384ec591c0955e994c5d5932214c8d321
dyb-dev <2737993954@qq.com>
2025-06-24 00:34:29 +0800

    ✨ feat: 新增interaction相关hooks

--- docs/src/hooks/index.ts
+++ docs/src/hooks/index.ts
@@ -2,10 +2,12 @@
  * @Author: dyb-dev
  * @Date: 2025-06-21 14:26:40
  * @LastEditors: dyb-dev
- * @LastEditTime: 2025-06-21 14:26:48
+ * @LastEditTime: 2025-06-24 00:28:41
  * @FilePath: /component-lib-template/docs/src/hooks/index.ts
  * @Description: hook模块
  */
 
+/** 导出用户交互相关hooks函数 */
+export * from "./interaction"
 /** 导出滚动控制相关hooks函数 */
 export * from "./scroll"
